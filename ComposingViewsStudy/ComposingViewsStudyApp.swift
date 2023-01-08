b4b35b0a7cf8b9db440d113c164ce84614c85d2c
ViniciusCabral <cabral.vm@outlook.com>
2023-01-07 22:21:55 -0300

    Beginning of studies on ComposingView

---
+++ ComposingViewsStudy/ComposingViewsStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposingViewsStudyApp.swift
+//  ComposingViewsStudy
+//
+//  Created by Vinicius Cabral on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ComposingViewsStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
