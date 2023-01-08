9e9b877435e43b9eea7b3ecd4b27810a34ccc720
ViniciusCabral <cabral.vm@outlook.com>
2023-01-07 23:16:31 -0300

    final coding for study on ComposingView

---
+++ ComposingViewsStudy/CodigoCOMComposingView/Phrasee.swift
@@ -0,0 +1,8 @@
+//
+//  Phrasee.swift
+//  ComposingViewsStudy
+//
+//  Created by Vinicius Cabral on 07/01/23.
+//
+
+import Foundation
---
+++ ComposingViewsStudy/CodigoCOMComposingView/PhraseeRowView.swift
@@ -0,0 +1,20 @@
+//
+//  PhraseeRowView.swift
+//  ComposingViewsStudy
+//
+//  Created by Vinicius Cabral on 07/01/23.
+//
+
+import SwiftUI
+
+struct PhraseeRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhraseeRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhraseeRowView()
+    }
+}
